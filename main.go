ff9b729f18db6fe84ddd673118d163bca3cba05c
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-05 23:35:54 +0100

    Add stuff about Jurassic Park

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 
 	//Launch Build processor
 	// Deploysaurus == old name for Deployer
+	// 3 deploysaurus in the kitchen, that's one more than in Jurassic Park
+	// We should be able to catch and eat the children
 	events := make(chan deploysaurus.Event, 1024)
 	for w := 1; w <= 3; w++ {
 		go deploysaurus.Deploysaurus(w, events)
